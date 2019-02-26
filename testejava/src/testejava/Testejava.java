c62874efd39b897f3db6c31ebee6575f5538bdd6
Laboratório-00-00 <Laboratrio-00-00@192.168.231.122>
2019-02-26 20:03:32 -0300

    projeto ALTERADO

--- testejava/src/testejava/Testejava.java
+++ testejava/src/testejava/Testejava.java
@@ -7,7 +7,7 @@ public class Testejava {
    
     
     public static void main(String[] args) {
-       System.out.println("PODEROSA LINDA DE PROGRAMAÇÃO");
+       System.out.println("PROJETO ALTERADO");
     }
     
 }
